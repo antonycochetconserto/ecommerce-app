a2ac6f931d846607502f9460ce9f6e96bb53cbd3
Antony Cochet <antony.cochet@conserto.pro>
2022-11-29 10:56:52 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-teal-700 text-8xl font-bold inline-block text-transparent bg-clip-text tracking-tighter p-2">
-      Hello world!
+      Hello world !
     </h1>
   );
 }
